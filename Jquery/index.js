8f03cf39a9830ebbf66d7e2224b916ac05c2ad83
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-06 01:52:02 +0530

    Add files via upload

--- Jquery/index.js
+++ Jquery/index.js
@@ -1,3 +1,9 @@
 $("h1").css("color", "red")
 
 $("div").addClass("block para")
+
+$("p").html("<h3> Dios Amigos </h3> ")
+
+$("button").click(function(){
+    $("body").css("background-image", "url(img/download.png)")
+})
